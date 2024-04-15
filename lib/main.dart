c2e37e2a5ad694652aa314e84c9bbf371fa6d168
Jean Marte <jeanmarte22@gmail.com>
2024-04-14 22:39:02 -0400

    update funciones jean 0.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'pages/inicio.dart';
 import 'pages/historia.dart';
@@ -34,19 +35,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Defensa Civil App',
       theme: ThemeData(
         primarySwatch: miColorPrimario,
-        
       ),
       initialRoute: '/',
       routes: {
         '/': (context) => MainScreen(),
-        '/inicio': (context) => InicioScreen(),
         '/historia': (context) => HistoriaScreen(),
         '/servicios': (context) => ServiciosScreen(),
         '/noticias': (context) => NoticiasScreen(),
@@ -69,11 +66,12 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 255, 111, 0),
-        title: const Text('Menú Principal',style: TextStyle(color: Colors.white),),
-        
+        title: const Text(
+          'Inicio',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       drawer: Drawer(
         child: ListView(
@@ -84,16 +82,12 @@ class MainScreen extends StatelessWidget {
                 color: miColorPrimario,
               ),
               child: Text(
-                'Menú Principal',
+                'Inicio',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 24,
                 ),
               ),
-            ),
-            MenuOption(
-              text: 'Inicio',
-              route: '/inicio',
             ),
             MenuOption(
               text: 'Historia',
@@ -143,9 +137,112 @@ class MainScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: const Center(
-        child: Text('Contenido de la pantalla principal.'),
+      body: Center(
+  child: Column(
+    children: [
+      // Título del CarouselSlider
+      Container(
+         padding: EdgeInsets.only(top: 40.0, bottom: 40.0, left: 10), // Margen vertical de 20.0
+        child: Text(
+          
+          'Acciones importantes de la Defensa Civil:',
+          style: TextStyle(
+            color: Color(0xFF004C98),
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
+      // CarouselSlider con los Card
+      CarouselSlider(
+        options: CarouselOptions(height: 400.0),
+        items: [
+          // Slider 1
+          Card(
+            color: Color(0xFF004C98),
+            child: Container(
+              margin: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.network(
+                    'https://presidencia.gob.do/sites/default/files/styles/large/public/news/2022-10/310690518_170442345577808_2718689553182378246_n%20%281%29.jpg?itok=4ZF7puvW',
+                    height: 200,
+                    width: 400,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Defensa Civil apuesta al uso de la tecnología para ser más eficiente ante cualquier fenomeno',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Slider 2
+          Card(
+            color: Color(0xFF004C98),
+            child: Container(
+              margin: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.network(
+                    'https://aplatanaonews.com/wp-content/uploads/2023/11/FOTO-2-631x560.jpg',
+                    height: 200,
+                    width: 400,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Defensa Civil rescata siete personas con vida tras crecida del Río Fula, Monseñor Nouel',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Slider 3
+          Card(
+            color: Color(0xFF004C98),
+            child: Container(
+              margin: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.network(
+                    'https://i0.wp.com/nuevodiario-assets.s3.us-east-2.amazonaws.com/wp-content/uploads/2023/11/19173248/WhatsApp-Image-2023-11-19-at-9.20.31-PM-1.jpeg?resize=640%2C480&quality=100&ssl=1',
+                    height: 200,
+                    width: 400,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Defensa Civil rescata una familia estaba atrapada por crecida de río Duey en Higüey',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    ],
+  ),
+),
     );
   }
 }
